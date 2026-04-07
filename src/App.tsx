9e6a8e39303d0fc19afa2e4f9b3da113410826ea
Yuri Ch <Yurich@MacBook-Air-5.local>
2026-04-07 11:24:11 +0300

    Restore original site version

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
-import RetreatDeck from './RetreatDeck';
+import YuryEventPage from './components/YuryEventPage';
 import './index.css';
 
 export default function App() {
-  return <RetreatDeck />;
+  return (
+    <div className="relative min-h-screen">
+      <YuryEventPage />
+    </div>
+  );
 }
